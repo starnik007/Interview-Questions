d4e87982c6c7b6f19b1a998e7989b0b0ca6c9d61
saurabh kumar pandey <80593478+Aagd12@users.noreply.github.com>
2022-10-12 14:25:30 +0530

    Add files via upload
    Cut_Ribbon_Problem

---
+++ A_Cut_Ribbon.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vll vector<int>
+#define pi (3.141592653589)
+#define mod 1000000007
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define max3(a, b, c) max(c, max(a, b))
+#define max4(a, b, c, d) max(d, max(c, max(a, b)))
+#define rfo(i, j, n) for (int i = n - 1; i >= j; i--)
+#define fo(i, j, n) for (int i = j; i < n; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+    if (n == 2)
+        return true;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int GCD(int a, int b)
+{
+    while (b)
+    {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+int LCM(int a, int b)
+{
+    return (a * b) / GCD(a, b);
+}
+int32_t main()
+{
+    int n, a, b, c, sum = INT_MIN;
+    cin >> n;
+    cin >> a >> b >> c;
+    int m = n / min3(a, b, c)+1;
+    fo(i, 0, m)
+    {
+        fo(j, 0, m)
+        {
+            if ((n - b * i - c * j) >= 0)
+            {
+                if ((n - b * i - c * j) / a * a == (n - b * i - c * j))
+                {
+                    sum = max(sum, (n - b * i - c * j) / a + i + j);
+                }
+            }
+        }
+    }
+    cout << sum << endl;
+
+    return 0;
+}
